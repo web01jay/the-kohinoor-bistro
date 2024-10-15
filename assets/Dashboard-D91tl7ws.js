04f3ec71fefeee8552dff05f35eff3bbddd2baef
Jay Panchal <jaypanchal@Jays-Mac-mini.local>
2024-10-15 09:25:25 +0530

    New Build

---
+++ assets/Dashboard-D91tl7ws.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D3bOH2Mo.js";const o=()=>r.jsx("h1",{children:"Dashboard - Protected"});export{o as default};
---
+++ assets/Notification-BpLjri8x.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D3bOH2Mo.js";const o=()=>t.jsx("div",{children:"Notification"});export{o as default};
